03bc358f2d7df83e5c464f109a245500dedc7f66
Labeb Refat <labebrefat888@gmail.com>
2025-03-16 20:00:52 +0100

    Added C_programmig files

---
+++ C_programming/Lecture_3_Assignment/Nested_For_Loop/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: 16 mar 2025
+ *      Author: lebad
+ */
+#include "stdio.h"
+
+
+void main ()
+{
+	int i=0;
+	int j=0;
+	for (i=0; i<10; i++)
+	{
+
+		for(j=i; j<10;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+
+	}
+}
